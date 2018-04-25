708ee705d699d9e8de1e95ea2ad0c2a0092d8bca
Vitalii Rybalko <vitalii.rybalko@globallogic.com>
2018-04-25 20:31:47 +0300

    works somehow

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from random import randint
 
 INF=10e15
 
@@ -53,6 +54,18 @@ class State:
                 if self.state[i][j] == 0:
                     return i, j
 
+    def makeOneRandomMove(self):
+        empty_coord = self.findEmpty()
+        i = randint(0, 3)
+        if i == 0 and empty_coord[0] > 0:
+            return self._makeMove(empty_coord, [-1, 0])
+        elif i == 1 and empty_coord[0] < len(self.state) - 1:
+            return self._makeMove(empty_coord, [ 1, 0])
+        elif i == 2 and empty_coord[1] > 0:
+            return self._makeMove(empty_coord, [ 0,-1])
+        elif i == 3 and empty_coord[1] < len(self.state) - 1:
+            return self._makeMove(empty_coord, [ 0, 1])
+
     def makeMoves(self):
         empty_coord = self.findEmpty()
         moves = []
@@ -97,32 +110,38 @@ class State:
     def __lt__(self, value):
         # print("------------------------------------------------------------")
         # print("performing comparsion")
+        # print(self, value)
         # print(F_SCORE.get(self, INF), F_SCORE.get(value, INF))
         # print("------------------------------------------------------------")
         return F_SCORE.get(self, INF) < F_SCORE.get(value, INF)
 
-from sortedcontainers import SortedDict
+from bisect import bisect_left, bisect_right
 
 F_SCORE=None
 
 class HashSet:
     def __init__(self):
         self.table = {}
-        # self.table = SortedDict({})
+        self.keys_list = []
 
     def add(self, elem):
-        # print("table items before insert: ", self.table.items())
+        # print("list before add: ", self.keys_list)
         self.table[elem] = 1
-        # print("table items after insert: ", self.table.items())
+        pos = bisect_left(self.keys_list, elem)
+        if pos == len(self.keys_list):
+            self.keys_list.append(elem)
+        else:
+            self.keys_list.insert(pos, elem)
+        # print("list after add: ", self.keys_list)
 
     def getElem(self):
-        # return self.table.keys()[0]
-        for key in self.table.keys():
-            return key
+        return self.keys_list[0]
+        # for key in self.table.keys():
+        #     return key
 
     def __iter__(self):
         yield next(iter(self.table))
-    
+
     def __len__(self):
         return len(self.table)
 
@@ -135,14 +154,22 @@ class HashSet:
             return False
 
     def remove(self, elem):
-        # print("table items before remove: ", self.table.items())
         if elem in self.table:
-            # print("removing: ", elem, "from table:", self.table)
             self.table.pop(elem)
-        # print("table items after remove: ", self.table.items())
-        # if elem in self.table:
-        #     raise ValueError("NOT REMOVED ELEM")
-        # print("table after removing: ", self.table)
+
+        # print("elem: ", elem)
+        # print("list before remove: ", self.keys_list)
+        pos = self.keys_list.index(elem)
+        if pos == len(self.keys_list):
+            pos -= 1
+            # print("pos: ", pos);
+            if self.keys_list[pos] != elem:
+                raise KeyError("Value " + repr(elem) + " not in HashSet")
+        self.keys_list.pop(pos)
+        # print("list after remove: ", self.keys_list, pos)
+
+        if elem in self.table:
+            raise ValueError("NOT REMOVED ELEM")
 
 def get_with_default(container, key, default):
     try:
@@ -151,14 +178,15 @@ def get_with_default(container, key, default):
         return default
 
 def select_optimal_state(f_score, states, solved_state):
+    # optimal = states.getElem()
+    # optimal_score = f_score.get(optimal, INF)
+    # for state in states:
+    #     state_f_score = f_score.get(state, INF)
+    #     if state_f_score < optimal_score:
+    #         optimal = state
+    #         optimal_score = state_f_score
+    # print("optimal_score: ", optimal_score)
     optimal = states.getElem()
-    optimal_score = f_score.get(optimal, INF)
-    for state in states:
-        state_f_score = f_score.get(state, INF)
-        if state_f_score < optimal_score:
-            optimal = state
-            optimal_score = state_f_score
-    print("optimal_score: ", optimal_score)
     return optimal
 
 def reconstruct_path(came_from, state):
@@ -174,6 +202,14 @@ def reconstruct_path(came_from, state):
     print("solution path:")
     for item in reversed(path):
         print(item)
+
+def shuffle(solved_state):
+    s = State(solved_state, 0)
+    for _ in range(0, 1000):
+        tmp = s.makeOneRandomMove()
+        if tmp:
+            s = tmp
+    return s
 
 def solve(initial_state, solved_state):
     global F_SCORE
@@ -214,8 +250,23 @@ def solve(initial_state, solved_state):
     print("cant solve")
 
 def main():
-    initial_state = [[0, 1, 2], [5, 6, 3], [4, 7, 8]]
-    solved_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    # initial_state = [[0, 1, 2], [5, 6, 3], [4, 7, 8]]
+    # initial_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    # solved_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    # initial_state = [
+    #         [ 0, 1, 2, 3],
+    #         [ 4, 5, 6, 7],
+    #         [ 8, 9,10,11],
+    #         [12,13,14,15],
+    #         ]
+    solved_state = [
+            [ 1, 2, 3, 4],
+            [ 5, 6, 7, 8],
+            [ 9,10,11,12],
+            [13,14,15, 0],
+            ]
+    initial_state = shuffle(solved_state).state
+    print(initial_state)
     solve(initial_state, solved_state)
 
 if __name__ == "__main__":
